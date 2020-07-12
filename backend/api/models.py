c3f2f0572ce79d22fd2f141845a2da43b5fe3782
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2020-07-11 18:17:07 -0700

    Added budget models.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,39 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class IdMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class StartEndMixin(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    class Meta:
+        abstract = True
+
+
+class MoneyMixin(models.Model):
+    amount = models.FloatField()
+    type = models.CharField(max_length=7, choices=(('income', 'income'), ('expense', 'expense')))
+
+    class Meta:
+        abstract = True
+
+
+class Budget(IdMixin, StartEndMixin):
+    pass
+
+
+class LineItem(IdMixin, StartEndMixin, MoneyMixin):
+    budget = models.ForeignKey(Budget, null=True, on_delete=models.SET_NULL)
+
+
+class Transaction(IdMixin, MoneyMixin):
+    line_item = models.ForeignKey(LineItem, null=True, on_delete=models.SET_NULL)
+    date = models.DateField(auto_now_add=True)
