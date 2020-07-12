62f90922489618b47e576f9b0c27d2f6ae03feb2
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2020-07-12 14:20:33 -0700

    Added validation for date in transactions against budget dates.

--- backend/api/serializers/serializer_transaction.py
+++ backend/api/serializers/serializer_transaction.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
+from api import common_text as text
 from api import models
 
 
@@ -14,3 +16,11 @@ class TransactionSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         validated_data['owner'] = request.user
         return super().create(validated_data)
+
+    def validate(self, attrs):
+        # validate the date with budget start and end date
+        date = attrs.get('date')
+        budget = attrs.get('line_item').budget
+        if date < budget.start_date or date > budget.end_date:
+            raise ValidationError(text.template_date_out_of_range.format(date, budget.start_date, budget.end_date))
+        return attrs
