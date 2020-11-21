d7941d45cdf30112f90610f618eb6627038f7574
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2020-11-21 02:07:54 -0800

    Added all admin models and default auth class for manual testing.

--- budget_project/urls.py
+++ budget_project/urls.py
@@ -1,8 +1,9 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('budget/', include('budget.urls'))
+    url('budget/', include('budget.urls'))
 ]
